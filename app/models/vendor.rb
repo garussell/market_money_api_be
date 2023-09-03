38f243fac8a927063b3dbe591c96761b2d980a1d
Allen Russell <garyrusselljr@gmail.com>
2023-09-03 08:57:52 -0600

    feat: add model method states_sold_in

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -3,8 +3,13 @@ class Vendor < ApplicationRecord
                         :description, 
                         :contact_name, 
                         :contact_phone 
+                        
   validates :credit_accepted, inclusion: { in: [true, false]}
 
   has_many :market_vendors
   has_many :markets, through: :market_vendors
+
+  def states_sold_in
+    markets.distinct.pluck(:state)
+  end
 end
--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Vendor, type: :model do
+  before do
+    @vendors = create_list(:vendor, 10)
+    @markets = create_list(:market, 10)
+    @market_vendors = []
+      
+    10.times do |i|
+      @market_vendors << create(:market_vendor, market: @markets[i], vendor: @vendors[i])
+    end
+  end
+
   describe "relationships" do
     it { should have_many(:market_vendors) }
     it { should have_many(:markets).through(:market_vendors) }
@@ -14,5 +24,11 @@ RSpec.describe Vendor, type: :model do
     it { should allow_value(true).for(:credit_accepted) }
     it { should allow_value(false).for(:credit_accepted) }
     it { should_not allow_value(nil).for(:credit_accepted) }
+  end
+
+  describe "states_sold_in" do
+    it "returns an array of states vendor sells in" do
+      expect(@vendors.first.states_sold_in).to be_an(Array)
+    end
   end
 end
